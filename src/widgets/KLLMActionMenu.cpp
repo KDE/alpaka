06785ce2eda20f53531daeb054f1a647f275b748
Laurent Montel <montel@kde.org>
2023-12-11 13:18:16 +0100

    Prepare to add KLLMActionMenu

---
+++ src/widgets/KLLMActionMenu.cpp
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+//
+// SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+
+#include "KLLMActionMenu.h"
+#include <KLocalizedString>
+#include <QMenu>
+
+using namespace KLLMWidgets;
+KLLMActionMenu::KLLMActionMenu(QObject *parent)
+    : KActionMenu{parent}
+{
+    setText(i18nc("List of message viewer closed", "Closed Reader"));
+    delete menu();
+    auto subMenu = new QMenu;
+    setMenu(subMenu);
+}
+
+KLLMActionMenu::~KLLMActionMenu() = default;
+
+void KLLMActionMenu::updateMenu()
+{
+}
---
+++ src/widgets/KLLMActionMenu.h
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+//
+// SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+
+#pragma once
+
+#include "kllmwidgets_export.h"
+#include <KActionMenu>
+
+namespace KLLMWidgets
+{
+/**
+ * @brief The KLLMActionMenu class
+ */
+class KLLMWIDGETS_EXPORT KLLMActionMenu : public KActionMenu
+{
+    Q_OBJECT
+public:
+    explicit KLLMActionMenu(QObject *parent = nullptr);
+    ~KLLMActionMenu() override;
+
+private:
+    KLLMWIDGETS_NO_EXPORT void updateMenu();
+};
+}
