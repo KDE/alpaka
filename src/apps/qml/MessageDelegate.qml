KDE/alpaka

commit 33ea097b7512b7fa536fe1965447c0c71edccf6b

Improve strings for i18n
I used an [en-dash instead of a hyphen](https://develop.kde.org/hig/text_and_labels/#symbols) for the date range in the copyright statement.

Additionally, I added some [contexts](https://api.kde.org/frameworks/ki18n/html/prg_guide.html#good_ctxt) to strings where there might be a "false friend" when translating them, or variables needed some explanation. Please make sure the contexts are actually correct, since I am unfamiliar with the code, and I am not the best at coding to begin with... Thanks!

src/apps/qml/MessageDelegate.qml
@@ -102,13 +102,13 @@ RowLayout {
                 Labs.Menu {
                     id: contextMenu
                     Labs.MenuItem {
-                        text: i18n("Copy")
+                        text: i18nc("@action:inmenu", "Copy")
                         shortcut: StandardKey.Copy
                         enabled: textEdit.selectedText.length > 0
                         onTriggered: textEdit.copy()
                     }
                     Labs.MenuItem {
-                        text: i18n("Select All")
+                        text: i18n("@action:inmenu", "Select All")
                         shortcut: StandardKey.SelectAll
                         onTriggered: textEdit.selectAll()
                     }
@@ -124,7 +124,7 @@ RowLayout {
             Controls.Label {
                 Layout.fillWidth: true
                 visible: AlpakaSettings.showDebugInfo && messageDelegate.sender === ChatModel.LLM && messageDelegate.finished
-                text: i18n("%1 tok/s · %2 tokens · %3 seconds",
+                text: i18nc("'toks' is short for 'tokens'", "%1 tok/s · %2 tokens · %3 seconds",
                            messageDelegate.tokensPerSecond.toFixed(2),
                            messageDelegate.tokenCount,
                            messageDelegate.duration.toFixed(2))

src/apps/main.cpp
@@ -48,7 +48,7 @@ int main(int argc, char *argv[])
                      QStringLiteral(ALPAKA_VERSION_STRING),
                      i18n("Ollama client"),
                      KAboutLicense::GPL_V3,
-                     i18n("© 2023-2025 Loren Burkholder"));
+                     i18n("© 2023–2025 Loren Burkholder"));
     about.addAuthor(i18n("Loren Burkholder"), i18n("Maintainer"), QStringLiteral("computersemiexpert@outlook.com"));
     about.addCredit(i18n("Laurent Montel"), i18n("Developer"), QStringLiteral("montel@kde.org"));
     about.setOrganizationDomain("kde.org");

src/core/KLLMInterface.cpp
@@ -134,7 +134,7 @@ void KLLMInterface::reload()
     auto rep = m_manager->get(req);
     m_ollamaCheck = connect(rep, &QNetworkReply::finished, this, [this, rep] {
         if (rep->error() != QNetworkReply::NoError) {
-            Q_EMIT errorOccurred(i18n("Failed to connect to interface at %1: %2", m_ollamaUrl, rep->errorString()));
+            Q_EMIT errorOccurred(i18nc("%1 is an URL, %2 an error description", "Failed to connect to interface at %1: %2", m_ollamaUrl, rep->errorString()));
             m_hasError = true;
             Q_EMIT readyChanged();
             Q_EMIT hasErrorChanged();

commit b5cf163db9cf97333db351ee1e3637ec75213bde

FIX menu item display GIT_SILENT
Fixed the problem caused by 33ea097.

src/apps/qml/MessageDelegate.qml
@@ -108,7 +108,7 @@ RowLayout {
                         onTriggered: textEdit.copy()
                     }
                     Labs.MenuItem {
-                        text: i18n("@action:inmenu", "Select All")
+                        text: i18nc("@action:inmenu", "Select All")
                         shortcut: StandardKey.SelectAll
                         onTriggered: textEdit.selectAll()
                     }